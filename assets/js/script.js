b89f7a13ff1435a2abbf2a849be072210823ae95
Ana-Niko-byte <ananikolayenia@gmail.com>
2023-12-03 19:45:36 +0100

    Add name validation + button event listeners JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,65 @@
 // javascript script for the math-tab-mastery game
-console.log('testing');
+
+/**
+ * This function validates the name the user inputs 
+ * into the 'name' input field on the first page.
+ * This is later used on the second page of the game 
+ * as a greeting.
+ */
+function nameValidator(){
+    let userName = document.getElementById('name').value;
+    let error = document.getElementsByTagName('span')[0];
+    // regex format acquired from url:https://stackoverflow.com/questions/18042133/check-if-input-is-number-or-letter-javascript
+    let regex=/^[a-zA-Z]+$/;
+
+    // if 'name' input has no value, add an error message, 
+    // otherwise, remove the text in the error message
+    if (!userName){
+        error.innerText = 'name required to continue';
+    } else if (!userName.match(regex)){
+        error.innerText = 'please use letters only'
+    } else {
+        error.innerText = '';
+        return true;
+    }
+}
+
+/**
+ * This function handles category selection through user interaction.
+ * The selected button (category) displays the type or arithemetic game
+ * on the second page.
+ */
+nameValidator(); 
+
+function categorySelection(){
+    // this returns an array of the category buttons
+    let buttons = document.getElementsByClassName('button-category');
+
+    // this loops through each of the buttons and attaches a 'click' event listener
+    for (let button of buttons){
+        // on click, it takes note of the value of the 'data-type' attribute on each of the buttons
+        button.addEventListener('click', function(){
+            let chosenCategory = this.getAttribute('data-type');
+            console.log(chosenCategory);
+
+            // this will return either true or false depending on whether there is a value in the name input field
+            let nameValidated = nameValidator();
+
+            // the game will only begin if the name field is valid
+            if (nameValidated){
+                beginGame(chosenCategory);
+            }
+        })
+    }
+}
+
+/**
+ * 
+ * This function begins the game with the chosen category from the user. 
+ */
+function beginGame(category){
+    // first we need to set the display of the second screen and hide the first.
+    console.log('the game has begun!');
+}
+
+categorySelection();
